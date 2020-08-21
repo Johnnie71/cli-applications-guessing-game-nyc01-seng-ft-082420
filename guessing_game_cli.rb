e857e2c2fae1410f5376f68c90a16a19f9b5a3f8
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-21 11:48:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   roll = 1 + rand(6)
 	user_input = gets.chomp
 	over = "exit"
-    if user_input == roll
+    if user_input == roll.to_s
     	puts "You guessed the correct number!"
     elsif user_input != roll && user_input != over
     	puts "Sorry! The computer guessed #{roll}."
